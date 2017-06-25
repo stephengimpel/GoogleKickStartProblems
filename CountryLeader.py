421b1611dff2e0479b7bf611104242c88de1a7c3
Landsharks <gimpelstephen1@gmail.com>
2017-06-24 19:58:45 -0700

    Dun goofed with find leader

--- CountryLeader.py
+++ CountryLeader.py
@@ -1,18 +1,29 @@
 #Reads each line of the file and stores it in a list
 def readFile(file):
-	fullList = list()
+	namesList = list()
 	with open(file) as f:
 		f.readline()
 		numberOfNames = int(f.readline())
 		for i in range(numberOfNames):
-			fullList.append(i)
-	return fullList
+			namesList.append(f.readline().replace('\n',''))
+		leader = findLeader(namesList)
+		print(leader)
 
 def findLeader(namesList):
-	
+	currentLeader = list()
+	currentLeader.append(namesList.pop())
+	for i in namesList:
+		if(len(currentLeader[0]) < len(i)):
+			currentLeader.clear()
+			currentLeader.append(i)
+		elif(len(currentLeader[0]) == len(i)):
+			currentLeader.append(i) 
+
+	return currentLeader
+
 
 
 def main():
-	print("stub")
+	readFile('temp.in')
 
 main()
