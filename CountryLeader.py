2069bbacce36736a9aa0e76bd36ed4ca0fac2530
David Tomassi <davidtomassi@Davids-MacBook-Pro.local>
2017-06-24 19:52:23 -0700

    Added countDistinctLetters function

--- CountryLeader.py
+++ CountryLeader.py
@@ -1,3 +1,5 @@
+import string
+
 #Reads each line of the file and stores it in a list
 def readFile(file):
 	fullList = list()
@@ -11,6 +13,16 @@ def readFile(file):
 def findLeader(namesList):
 	
 
+def countDistinctLetters(names):
+	alaphabet[26] = 0
+	count = 0
+	size = len(names)
+	for i in range(size):
+		letterIndex = alphabet[string.lowercase.index(names[i])]
+		if == 0:
+			count += 1
+			letterIndex = 1
+	return count
 
 def main():
 	print("stub")
