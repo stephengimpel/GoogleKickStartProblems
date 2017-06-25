c63ebea6fbac52441a9816c45f66064a28a3ff45
LandSharks <gimpelstephen1@gmail.com>
2017-06-25 11:48:23 -0700

    prep

---
+++ C.py
@@ -0,0 +1,23 @@
+def readFile(file):
+	namesList = list()
+	outputList = list()
+	with open(file) as f:
+		f.readline()
+		for i in f:
+			count = int(i)
+			for j in range(count):
+				namesList.append(f.readline().replace('\n',''))
+			leader = findLeader(namesList)
+			namesList.clear()
+			outputList.append(leader[0])
+	return outputList	
+
+def formatOutput(outputList):
+	f = open("c.out", 'w')
+	for c, i in enumerate(outputList): 
+		output = "Case #{}: {}\n".format(c+1, i)
+		print(output)
+		f.write(output)
+
+def main():
+	readFile('c.in')
